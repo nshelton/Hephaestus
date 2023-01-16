a836107d8f54e212d0a7169a1396a2c4594066a0
nick <nshelton@gmail.com>
2023-01-15 16:08:34 -0800

    gui update

--- gui.js
+++ gui.js
@@ -2,10 +2,54 @@
 
 PlotterGUI = function() { 
 
+    this.setPenUpValue = function(val) {
+        this.upValueText.innerText = val
+        app.setPenUpValue(val)
+        plotter.setPenUp(Math.round(val)); 
+        saveSettings("upPosition", val)
+    }
+
+    this.setPenDownValue = function(val) {
+        this.downValueText.innerText = val
+        app.setPenDownValue(val)
+
+        
+    }
 
     this.init = function (app) {
-
+        this.app = app
         this.guiNode = document.getElementById("gui")
+
+        function makeButton(text, x, y, onclick) {
+            theButton = document.createElement("div")
+            theButton.style.backgroundColor = "#bbbbbb"
+            theButton.style.height = "30px"
+            theButton.style.textAlign = "center"
+            theButton.style.position = "absolute"
+            theButton.style.left = x
+            theButton.style.bottom = y
+            theButton.style.border = "1px solid black"
+            theButton.onclick = onclick
+            theButton.innerText = text
+            theButton.style.width = "100px"
+            return theButton
+        }   
+
+        function makeText(text, x, y) {
+            theButton = document.createElement("div")
+            theButton.style.height = "30px"
+            // theButton.style.textAlign = "center"
+            theButton.style.position = "absolute"
+            theButton.style.left = x
+            theButton.style.bottom = y
+            // theButton.style.border = "1px solid black"
+            // theButton.onclick = app.plot
+            theButton.innerText = text
+            theButton.style.width = "100px"
+            return theButton
+        }   
+
+
         
         this.progressbar = document.createElement("div")
         this.progressbar.style.backgroundColor = "#00ff00"
@@ -17,77 +61,58 @@ PlotterGUI = function() {
         this.queuebar.style.height = "10px"
         this.guiNode.appendChild(this.queuebar)
 
-        this.plotButton = document.createElement("div")
-        this.plotButton.style.backgroundColor = "#bbbbbb"
-        this.plotButton.style.height = "50px"
-        this.plotButton.style.textAlign = "center"
-        this.plotButton.style.position = "absolute"
-        this.plotButton.style.left = "0"
-        this.plotButton.style.bottom = "0"
-        this.plotButton.style.border = "1px solid black"
-        this.plotButton.onclick = app.plot
-        this.plotButton.innerText = "GO"
-        this.plotButton.style.width = "100px"
+        this.plotButton = makeButton("GO", 0, 0, app.plot)
+        this.plotButton.style.height = "100px"
         this.guiNode.appendChild(this.plotButton)
 
-        this.disengageButton = document.createElement("div")
-        this.disengageButton.style.backgroundColor = "#bbbbbb"
-        this.disengageButton.style.height = "30px"
-        this.disengageButton.style.textAlign = "center"
-        this.disengageButton.style.position = "absolute"
-        this.disengageButton.style.right = "0"
-        this.disengageButton.style.bottom = "0"
-        this.disengageButton.onclick = app.disengage
-        this.disengageButton.innerText = "disengage"
-        this.disengageButton.style.width = "100px"
-        this.disengageButton.style.border = "1px solid black"
+        this.disengageButton = makeButton("disengage", 200, 0, app.disengage)
         this.guiNode.appendChild(this.disengageButton)
 
-        this.penUp = document.createElement("div")
-        this.penUp.style.backgroundColor = "#bbbbbb"
-        this.penUp.style.height = "30px"
-        this.penUp.style.textAlign = "center"
-        this.penUp.style.position = "absolute"
-        this.penUp.style.right = "0"
-        this.penUp.style.bottom = "60px"
-        this.penUp.onclick = app.penUp
-        this.penUp.innerText = "up"
-        this.penUp.style.border = "1px solid black"
-        this.penUp.style.width = "100px"
-        this.guiNode.appendChild(this.penUp)
-        
-        this.penUpValue = document.createElement("input")
-        this.penUpValue.type = "range"
-        this.penUpValue.style.backgroundColor = "#bbbbbb"
-        this.penUpValue.style.height = "30px"
-        this.penUpValue.style.position = "absolute"
-        this.penUpValue.style.right = "200px"
-        this.penUpValue.style.bottom = "60px"
-        this.penUpValue.onclick = app.penUpValue
-        this.penUpValue.style.border = "1px solid black"
-        this.penUpValue.style.width = "200px"
-        this.guiNode.appendChild(this.penUpValue)
+        this.penUpButton = makeButton("penUp", 200, 50, app.penUp)
+        this.guiNode.appendChild(this.penUpButton)
 
-        this.penDown = document.createElement("div")
-        this.penDown.style.backgroundColor = "#bbbbbb"
-        this.penDown.style.height = "30px"
-        this.penDown.style.textAlign = "center"
-        this.penDown.style.position = "absolute"
-        this.penDown.style.right = "0"
-        this.penDown.style.bottom = "90px"
-        this.penDown.onclick = app.penDown
-        this.penDown.style.border = "1px solid black"
-        this.penDown.innerText = "down"
-        this.penDown.style.width = "100px"
-        this.guiNode.appendChild(this.penDown)
-        
+        this.penDownButton = makeButton("penDown", 200, 100, app.penDown)
+        this.guiNode.appendChild(this.penDownButton)
 
+        this.penUpSlider = document.createElement("input")
+        this.penUpSlider.type = "range"
+        this.penUpSlider.style.backgroundColor = "#bbbbbb"
+        this.penUpSlider.style.height = "30px"
+        this.penUpSlider.style.position = "absolute"
+        this.penUpSlider.style.left = "350px"
+        this.penUpSlider.style.bottom = "100px"
+        this.penUpSlider.min = 0
+        this.penUpSlider.max = 33250
+        this.penUpSlider.onchange = (e) => {this.setPenUpValue(e.target.value)}
+        this.penUpSlider.style.width = "200px"
+        this.guiNode.appendChild(this.penUpSlider)
+
+        this.penDownSlider = document.createElement("input")
+        this.penDownSlider.type = "range"
+        this.penDownSlider.style.backgroundColor = "#bbbbbb"
+        this.penDownSlider.style.height = "30px"
+        this.penDownSlider.style.position = "absolute"
+        this.penDownSlider.style.left = "350px"
+        this.penDownSlider.style.bottom = "50px"
+        this.penDownSlider.min = 0
+        this.penDownSlider.max = 33250
+        this.penDownSlider.onchange = (e) => {this.setPenDownValue(e.target.value)}
+        this.penDownSlider.style.width = "200px"
+        this.guiNode.appendChild(this.penDownSlider)
+
+        
+        this.upValueText = makeText("upValue", 400, 30)
+        this.guiNode.appendChild(this.upValueText)
+
+        this.downValueText = makeText("downValue", 400, 80)
+        this.guiNode.appendChild(this.downValueText)
 
         this.debugText = document.createElement("div")
 
         this.guiNode.appendChild(this.debugText)
 
     }
+
 
 
     this.update = function (queue, plotter) {
